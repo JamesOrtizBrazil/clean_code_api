c1c7da4d6fc0d6b3f644956d1e3730f20d8d6496
JamesOrtizBrazil <jamesortiz@gmail.com>
2024-10-14 17:12:41 -0300

    chore: update eslint lib

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        extends: 'standard-with-typescript',
+        parserOptions: {
+            "project": "./tsconfig.json"
+        },
+        "rules": {
+            "@typescript-eslint/strict-boolean-expressions": "off",
+        },
+        ignores: ['node_modules/**', 'dist/**'],
+    },
+];
+
