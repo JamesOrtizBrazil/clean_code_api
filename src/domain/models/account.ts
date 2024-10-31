23b0d77e671fa81d5955b67d4d895674bf96ce39
JamesOrtizBrazil <jamesortiz@gmail.com>
2024-10-31 10:19:30 -0300

    refactor: change models to use type alias instead of interface

--- src/domain/models/account.ts
+++ src/domain/models/account.ts
@@ -1,7 +1,6 @@
-export interface AccountModel {
+export type AccountModel = {
   id: string
   name: string
   email: string
   password: string
-  accessToken?: string // eu só consegui fazer colocando o accessToken no model, mas não sei se é a melhor forma
 }
