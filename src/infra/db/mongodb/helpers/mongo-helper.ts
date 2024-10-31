2259eb1affca9650772f6cae91bdbfa611123952
JamesOrtizBrazil <jamesortiz@gmail.com>
2024-10-31 14:23:08 -0300

    feat: ensure SurveyResultMongoRepository updatesw surveyResult if its not new

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -23,7 +23,7 @@ export const MongoHelper = {
 
   map: (data: any): any => {
     const { _id, ...rest } = data
-    return { ...rest, id: _id.toHexString() }
+    return { ...rest, id: _id }
   },
 
   mapCollection: (collection: any[]): any[] => {
