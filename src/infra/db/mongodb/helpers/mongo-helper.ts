1c6cfac89cd5b7f61d8a5787a6a4a9ca42d10428
JamesOrtizBrazil <jamesortiz@gmail.com>
2024-10-31 14:29:33 -0300

    fix: ensure SurveyMongoRepository uses MongoHelper.map in all methods

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -22,8 +22,8 @@ export const MongoHelper = {
   },
 
   map: (data: any): any => {
-    const { _id, ...rest } = data
-    return { ...rest, id: _id }
+    const { _id, ...collectionWithoutId } = data
+    return Object.assign({}, collectionWithoutId, { id: _id })
   },
 
   mapCollection: (collection: any[]): any[] => {
